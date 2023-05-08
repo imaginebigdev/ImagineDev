36445b72ac0fb023bcdfe4b8b61d448fc978992b
gaspar <gasparmunoz@hotmail.com>
2023-05-08 14:48:45 -0300

    cambio en about home

--- src/components/App/About/SecondContent.jsx
+++ src/components/App/About/SecondContent.jsx
@@ -1,4 +1,3 @@
-
 const SecondContent = ({ accordions, rtl }) => {
   return (
     <div className="content sec-content">
@@ -7,46 +6,46 @@ const SecondContent = ({ accordions, rtl }) => {
           <div className="col-lg-5 order-2 order-lg-0">
             <div className="info">
               <div className="section-head style-4">
-                <small className="title_small">{ rtl ? 'إدارة أفضل للملاحظات' : 'Better Note Management' }</small>
-                <h2 className="mb-30">{ rtl ? 'ملاحظاتك' : 'Your Notes' } <span> { rtl ? 'في امان' : 'Security' } </span></h2>
+                <small className="title_small">
+                  {rtl
+                    ? "إدارة أفضل للملاحظات"
+                    : "We viralize on these platforms."}
+                </small>
+                <h2 className="mb-15">
+                  <span>
+                    {" "}
+                    <i className="fa fa-hashtag"></i>{" "}
+                    {rtl ? "في امان" : "TRENDING"}{" "}
+                  </span>
+                  {rtl ? "ملاحظاتك" : ", Viralization Solution "}
+                  <small className="title_small">
+                    {rtl
+                      ? "إدارة أفضل للملاحظات"
+                      : "We know about viralization"}
+                  </small>
+                </h2>
               </div>
-              <p className="text mb-40">
-                {
-                 rtl ? 
-                 'يتزامن تلقائيًا عبر جميع أجهزتك. يمكنك أيضًا الوصول إلى الملاحظات وكتابتها بدون اتصال بالإنترنت'
-                 :
-                 'Automatically syncs across all your devices. You can also access and write notes without internet connection.' 
-                }
+              <p className="text mb-10">
+                {rtl
+                  ? "يتزامن تلقائيًا عبر جميع أجهزتك. يمكنك أيضًا الوصول إلى الملاحظات وكتابتها بدون اتصال بالإنترنت"
+                  : "Viralization is when a unit of information (audio or video) acquires a character of massive knowledge through a computer process of diffusion."}
               </p>
-              <div className="faq style-3 style-4">
-                <div className="accordion" id="accordionExample">
-                  {
-                    accordions.map((accordion, index) => (
-                      <div className="accordion-item" key={index}>
-                        <h2 className="accordion-header" id={`heading${accordion.id}`}>
-                          <button className={`accordion-button ${index !== 0 ?'collapsed':''}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${accordion.id}`} aria-expanded="true" aria-controls={`collapse${accordion.id}`}>
-                            { accordion.title }
-                          </button>
-                        </h2>
-                        <div id={`collapse${accordion.id}`} className={`accordion-collapse collapse ${index === 0 ?'show':''}`} aria-labelledby={`heading${accordion.id}`} data-bs-parent="#accordionExample">
-                          <div className="accordion-body">
-                            { accordion.content }
-                          </div>
-                        </div>
-                      </div>
-                    ))
-                  }
-                </div>
-              </div>
-              <a href="https://chrome.google.com/webstore/category/extensions" rel="noreferrer" className="btn btn-img mt-40 rounded-pill" target="_blank">
-                <div className="icon img-contain">
-                  <img src="/assets/img/icons/chrome_icon.png" alt="" />
-                </div>
-                <div className="inf">
-                  <small>{ rtl ? 'متاح في' : 'Available in the' }</small>
-                  <h6>{ rtl ? 'متجر كروم الالكتروني' : 'Chrome Web Store' }</h6>
-                </div>
-              </a>
+              <p className="text mb-40">
+                <span style={{ color: "#5842bc" }}>Our TRENDING </span>
+                {rtl
+                  ? "يتزامن تلقائيًا عبر جميع أجهزتك. يمكنك أيضًا الوصول إلى الملاحظات وكتابتها بدون اتصال بالإنترنت"
+                  : " solution is based on mapping the scenario of possibilities that exist through different mass communication channels. It includes:"}
+              </p>
+              <ul>
+                {accordions.map((item, index) => (
+                  <li className="d-flex align-items-center mb-3" key={index}>
+                    <small className="icon-30 bg-gray rounded-circle color-blue4 d-inline-flex align-items-center justify-content-center me-3">
+                      <i className={item.icon}></i>
+                    </small>
+                    <h6 className="fw-bold">{item.title}</h6>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
           <div className="col-lg-6 order-0 order-lg-2">
@@ -56,9 +55,13 @@ const SecondContent = ({ accordions, rtl }) => {
           </div>
         </div>
       </div>
-      <img src="/assets/img/about/about_s4_bubble2.png" alt="" className="bubble2" />
+      <img
+        src="/assets/img/about/about_s4_bubble2.png"
+        alt=""
+        className="bubble2"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SecondContent
+export default SecondContent;
